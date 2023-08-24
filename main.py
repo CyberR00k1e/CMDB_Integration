cc8f3fb50ad36171ee8793b1aefe86fe97ca0bc1
Sharat Nautiyal <85890750+CyberR00k1e@users.noreply.github.com>
2023-08-24 20:03:41 +0800

    Update main.py
    removed some test print statements

--- main.py
+++ main.py
@@ -17,21 +17,21 @@ for i in u:
 
     id.append(i["id"])
 
-print(id)
+
 notes=[]
 for l in id:
     s=requests.get(f"https://demo.vectra.io/api/v2.5/detections/{l}",headers={'Authorization': 'Token <<enter the token>>'})
     capture = s.json()
     src= capture['src_ip']
-    print(src)
+    
     for i in range(len(cmdb)):
         if src == cmdb[i][0]:
-            print("yes")
+           
             data=str(cmdb[i])
-            print(data)
+            
             x=data.replace("  ","\n")
             x=data.replace("\n","\n \n")
-            print(x)
+            
             x = requests.post(f"https://demo.vectra.io/api/v2.5/detections/{l}/notes",
                               headers={'Authorization': 'Token <<enter the token>>',
                                        "Content-Type": "application/json"}, json={"note": x})
